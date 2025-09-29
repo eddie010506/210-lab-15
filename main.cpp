57fdded2838f0496a43b25f4d473c3ed17875f39
Edward Choi <edwardchoi2001@gmail.com>
2025-09-28 20:03:53 -0700

    final push

--- main.cpp
+++ main.cpp
@@ -38,3 +38,51 @@ public:
     int getYearReleased() const {
         return yearReleased;
     }
+
+    string getTitle() const {
+        return title;
+    }
+
+    // prints the movie's data in the specified format.
+    void print() const {
+        cout << "Movie: " << screenwriter << endl;
+        cout << "Year released: " << yearReleased << endl;
+        cout << "Screenwriter: " << title << endl << endl;
+    }
+};
+
+int main() {
+    vector<Movie> movie_container;
+    ifstream inputFile("input.txt");
+
+    if (!inputFile) {
+        cerr << "Error: Could not open input file." << endl;
+        return 1;
+    }
+
+    string tempTitle, tempScreenwriter;
+    int tempYear;
+
+    // read from the file as long as there are lines to process.
+    while (getline(inputFile, tempTitle)) {
+        inputFile >> tempYear;
+        inputFile.ignore(numeric_limits<streamsize>::max(), '\n'); 
+        getline(inputFile, tempScreenwriter);
+
+        Movie tempMovie;
+        tempMovie.setTitle(tempTitle);
+        tempMovie.setYearReleased(tempYear);
+        tempMovie.setScreenwriter(tempScreenwriter);
+
+        movie_container.push_back(tempMovie);
+    }
+
+    inputFile.close();
+
+    // output the contents of the vector.
+    for (const auto& movie : movie_container) {
+        movie.print();
+    }
+
+    return 0;
+}
